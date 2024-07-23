1c8757fea21d7563df44927295a74dd612539013
rougemeister <rougemeister1@gmail.com>
2024-07-23 11:15:34 +0000

    Added add and greet funcitons

---
+++ script.js
@@ -0,0 +1,10 @@
+const greetUser = (name)=> `Hello ${name}`
+
+
+const add = (num1, num2)=> num1 + num2
+
+
+module.exports = {
+    greetUser,
+    add,
+}
---
+++ script.test.js
@@ -0,0 +1,9 @@
+const { greetUser, add,reverse } = require('./script');
+
+test('Greet user', () => {
+    expect(greetUser('John')).toBe('Hello John');
+});
+
+test('Add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+});
